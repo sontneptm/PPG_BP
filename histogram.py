cff044d169972fa4334dd860df8d4f1f7cb3e235
r4you96 <47077726+r4you96@users.noreply.github.com>
2020-11-14 22:37:10 +0900

    csv and etc
    hello world

--- histogram.py
+++ histogram.py
@@ -1,10 +1,12 @@
 from matplotlib import pyplot as plt
 from keras.models import load_model
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import numpy as np
+from tensorflow.keras.optimizers import Adam
 
 #불러올 모델명
-loaded_model = 'ppg_model.h5'
+loaded_model = 'ppg_gru_model.h5'
 bp_data = np.loadtxt('ppg_bp_encoded.csv', delimiter=',') #데이터 읽어옴
 
 y_data = bp_data[:, :2]
@@ -18,25 +20,16 @@ print('-------x reshape-----------')
 x_data = x_data.reshape((x_data.shape[0], x_data.shape[1], 1))
 
 model = load_model(loaded_model)
+model.compile(optimizer=Adam(lr=1.46e-3), loss='mse')
 # 3. 모델 사용하기
 yhat = model.predict(x_data) #예측 결과
 
-print(yhat.shape)
-print(yhat)
 sys_list = []
 dia_list = []
 for i in range(len(y_data)) :
     sys_list.append(yhat[i][0]-y_data[i][0])
     dia_list.append(yhat[i][1]-y_data[i][1])
 
-"""
-plt.figure(1)
-plt.title('systolic')
-plt.hist(sys_list, bins=100, density=True, alpha=0.7, histtype='step')
-plt.figure(2)
-plt.title('diastolic')
-plt.hist(dia_list, bins=100, density=True, histtype='stepfilled')
-"""
 plt.rcParams["figure.figsize"] = (7,5)
 plt.subplot(211)
 plt.title('systolic')
